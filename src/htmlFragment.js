266cb10fed85e287f89d4c4b731ca5c879d5ba2e
U-KAMAKURA\Aida <vaclav@navratil.vn>
2020-03-14 22:17:03 +0100

    style tweak

--- src/htmlFragment.js
+++ src/htmlFragment.js
@@ -7,6 +7,7 @@ var htmlFragment =  "<div id=\"plague-doctor\" style=\"" +
                         "height:calc(537vh/25);" +
                         "position:fixed;" +
                         "right:2vw;" +
+                        "z-index: 19;" +
                         "width:calc(494vh/25);\">" +
 
                         "<div class=\"plague-doctor-bubble\" style=\"" +
@@ -16,7 +17,7 @@ var htmlFragment =  "<div id=\"plague-doctor\" style=\"" +
                             "background-size:100% 100%;" +
                             "bottom:10vh;" +
                             "display:none;" +
-                            "max-width:25vw;" +
+                            "max-width:45vw;" +
                             "min-width:200px;" +
                             "position:fixed;" +
                             "right:calc((494vh/25) + 2vw);\">" +
@@ -25,7 +26,7 @@ var htmlFragment =  "<div id=\"plague-doctor\" style=\"" +
                                 "color:#2f2f2f;" +
                                 "font-family:'Fondamento','Ink Free',cursive;" +
                                 "font-size:3.0vh;" +
-                                "margin:35px 55px 35px 35px;" +
+                                "margin:45px 65px 45px 45px;" +
                                 "text-align:center;\">" +
                             "</div>" +
 
